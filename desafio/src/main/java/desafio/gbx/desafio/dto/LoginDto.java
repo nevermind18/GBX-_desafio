31e218a40d77f2c091a54aae6ed47ad8fd9b402a
brunfontoura805 <10070369@restinga.ifrs.edu.br>
2024-01-13 22:38:20 -0300

    adicao de login

---
+++ desafio/src/main/java/desafio/gbx/desafio/dto/LoginDto.java
@@ -0,0 +1,1 @@
+package desafio.gbx.desafio.dto;import lombok.AllArgsConstructor;import lombok.Data;import lombok.NoArgsConstructor;@Data@NoArgsConstructor@AllArgsConstructorpublic class LoginDto {    private String numeroConta;    private String senha;}
